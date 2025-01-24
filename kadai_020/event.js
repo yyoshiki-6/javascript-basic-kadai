5c36615812cb9f99fc021edb54946c2ecd5405c2
yyoshiki-6 <yy6br@icloud.com>
2025-01-24 19:26:19 +0900

    kadai_020をアップデート

---
+++ kadai_020/event.js
@@ -0,0 +1,11 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// textというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+  // id要素(text)を変換する
+  text.textContent = 'ボタンをクリックしました';
+});
